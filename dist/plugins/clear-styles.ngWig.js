d2f05993473e52ea43110e4cbea64a0b1b0a1f79
elgambet <gambeita@gmail.com>
2018-04-28 19:32:29 -0300

    ng-annotate plugin installed

--- dist/plugins/clear-styles.ngWig.js
+++ dist/plugins/clear-styles.ngWig.js
@@ -1,21 +1,26 @@
-'use strict';
-
-angular.module('ngWig').config(['ngWigToolbarProvider', function (ngWigToolbarProvider) {
-  ngWigToolbarProvider.addCustomButton('clear-styles', 'nw-clear-styles-button');
-}]).component('nwClearStylesButton', {
-  bindings: {
-    editMode: '=',
-    disabled: '=',
-    content: '='
-  },
-  template: '<button ng-click="$ctrl.clearStyles($event)" ng-disabled="$ctrl.editMode || $ctrl.disabled" class="nw-button clear-styles" title="Clear Styles">Clear Styles</button>',
-  controller: function controller() {
-    this.clearStyles = function (e) {
-      // create a virutal element to manipulate the content of the editor
-      var div = document.createElement('div');
-      div.innerHTML = this.content;
-      this.content = div.textContent;
-    };
-  }
-});
-//# sourceMappingURL=clear-styles.ngWig.js.map
+angular
+	.module('ngWig')
+	.config([
+		'ngWigToolbarProvider',
+		function(ngWigToolbarProvider) {
+			ngWigToolbarProvider.addCustomButton('clear-styles', 'nw-clear-styles-button');
+		}
+	])
+	.component('nwClearStylesButton', {
+		bindings: {
+			editMode: '=',
+			disabled: '=',
+			content: '='
+		},
+		template:
+			'<button ng-click="$ctrl.clearStyles($event)" ng-disabled="$ctrl.editMode || $ctrl.disabled" class="nw-button clear-styles" title="{{$ctrl.title}}">/button>',
+		controller: function($rootScope) {
+			this.title = $rootScope.ngWigTranslations.clearstyles || 'Clear Styles';
+			this.clearStyles = function(e) {
+				// create a virutal element to manipulate the content of the editor
+				var div = document.createElement('div');
+				div.innerHTML = this.content;
+				this.content = div.textContent;
+			};
+		}
+	});
--- dist/plugins/forecolor.ngWig.js
+++ dist/plugins/forecolor.ngWig.js
@@ -1,20 +1,24 @@
-'use strict';
-
-angular.module('ngWig').config(['ngWigToolbarProvider', function (ngWigToolbarProvider) {
-	ngWigToolbarProvider.addCustomButton('forecolor', 'nw-forecolor-button');
-}]).component('nwForecolorButton', {
-	bindings: {
-		execCommand: '=',
-		editMode: '=',
-		disabled: '='
-	},
-	template: '<button colorpicker type="button" ng-model="fontcolor" ng-disabled="$ctrl.editMode || $ctrl.disabled" colorpicker-position="right" class="nw-button font-color" title="Font Color">Font Color</button>',
-	controller: ["$scope", function ($scope) {
-		var _this = this;
-
-		$scope.$on('colorpicker-selected', function ($event, color) {
-			_this.execCommand('foreColor', color.value);
-		});
-	}]
-});
-//# sourceMappingURL=forecolor.ngWig.js.map
+angular
+	.module('ngWig')
+	.config([
+		'ngWigToolbarProvider',
+		function(ngWigToolbarProvider) {
+			ngWigToolbarProvider.addCustomButton('forecolor', 'nw-forecolor-button');
+		}
+	])
+	.component('nwForecolorButton', {
+		bindings: {
+			execCommand: '=',
+			editMode: '=',
+			disabled: '='
+		},
+		template:
+			'<button colorpicker type="button" ng-model="fontcolor" ng-disabled="$ctrl.editMode || $ctrl.disabled" colorpicker-position="right" class="nw-button font-color" title="{{$ctrl.title}}"></button>',
+		controller: function($rootScope, $scope) {
+			console.log($rootScope.ngWigTranslations);
+			this.title = $rootScope.ngWigTranslations.forecolor || 'Font Color';
+			$scope.$on('colorpicker-selected', ($event, color) => {
+				this.execCommand('foreColor', color.value);
+			});
+		}
+	});
